5200b595fbd7f325e3a85920e6c8381721b0dfa5
vagrant <vagrant@homestead>
2017-10-03 18:00:25 +0000

    消息

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use EasyWeChat\Message\Article;
+use EasyWeChat\Message\Image;
+use EasyWeChat\Message\Material;
+use EasyWeChat\Message\News;
+use EasyWeChat\Message\Video;
+use EasyWeChat\Message\Voice;
 use Illuminate\Http\Request;
 
 class WechatController extends Controller
@@ -21,19 +27,32 @@ class WechatController extends Controller
             switch($message->MsgType) {
                 case 'event':
                   return '收到事件消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的事件消息 <'.date('Y-m-d H:i:s', $createTime).'>' . PHP_EOV
-                      . withEvent($message->Event, $message);
+                      . $this->withEvent($message->Event, $message);
                   break;
                 case 'text':
-                  return '收到文字消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的文字消息 <'.date('Y-m-d H:i:s', $createTime).'>'
+                        $text = new Text();
+                        $text->content = '收到文字消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的文字消息 <'.date('Y-m-d H:i:s', $createTime).'>'
                             . '文本消息内容：' . $message->Content;
+                        return $text;
                   break;
                 case 'image':
-                  return '收到图片消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的图片消息 <'.date('Y-m-d H:i:s', $createTime).'>'
-                            . '图片链接：' . $message->PicUrl;
+                        $text = new Image();
+                        $text->media_id = $message->MediaId;
+                        // 公众号在收到用户发送图片消息时，会将该图片存放在http://mmbiz.qpic.cn/mmbiz_jpg站点.
+                        return $text;
                   break;
+                case 'video':
+                        $video = new Video();
+                        $video->media_id = $message->MediaId;
+                        $video->ThumbMediaId = $message->ThumbMediaId;
+                        $video->title = $message->Title;
+                        $video->description = $message->Description;
+                        return $video;
+                    break;
                 case 'voice':
-                  return '收到语音消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的语音消息 <'.date('Y-m-d H:i:s', $createTime).'>'
-                            . '语音消息媒体id：' . $message->MediaId . '语音格式：' . $message->Format . 'Recognition：' . $message->Recognition;
+                        $voice = new Voice();
+                        $voice->media_id = $message->MediaId;
+                  return $voice;
                   break;
                 case 'location':
                   return '收到坐标消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的坐标消息 <'.date('Y-m-d H:i:s', $createTime).'>'
@@ -44,6 +63,31 @@ class WechatController extends Controller
                   return '收到链接消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的链接消息 <'.date('Y-m-d H:i:s', $createTime).'>'
                             . '消息标题：' . $message->Title . ' 消息描述：' . $message->Description . '消息链接：' . $message->Url;
                   break;
+                case 'tw':
+                    $news = new News([
+                        'title' => '测试数据',
+                        'description' => '测试数据描述',
+                        'url' => 'www.baidu.com',
+                        'image' => 'https://i.pximg.net/c/600x600/img-master/img/2017/09/23/21/31/40/65101437_p0_master1200.jpg'
+                    ]);
+                    return $news;
+                    break;
+                case 'wz':
+                    $article = new Article([
+                        'title' => 'DkfWeChat',
+                        'author' => 'dkf',
+                        'content' => '测试数据,测试数据,测试数据,测试数据,测试数据,测试数据,测试数据,测试数据.',
+                        'thumb_media_id' => '04c3f38da9773912145e5d4afb198618377ae2e7.jpg',
+                        'digest' => '图文消息的摘要，仅有单图文消息才有摘要，多图文此处为空',
+                        'source_url' => 'www.dengkangfa.com',
+                        'show_cover' => 1
+                    ]);
+                    return $article;
+                    break;
+                case 'sc':
+                    $material = new Material('image', '04c3f38da9773912145e5d4afb198618377ae2e7.jpg');
+                    return $material;
+                    break;
                 default:
                   return '收到其他消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的其他消息 <'.date('Y-m-d H:i:s', $createTime).'>';
                   break;
