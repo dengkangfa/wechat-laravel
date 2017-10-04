7bedcbfb8aee0dd1d5d2161bae83793ad3590fe1
vagrant <vagrant@homestead>
2017-10-04 04:00:53 +0000

    修复回复BUG

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -6,6 +6,7 @@ use EasyWeChat\Message\Article;
 use EasyWeChat\Message\Image;
 use EasyWeChat\Message\Material;
 use EasyWeChat\Message\News;
+use EasyWeChat\Message\Text;
 use EasyWeChat\Message\Video;
 use EasyWeChat\Message\Voice;
 use Illuminate\Http\Request;
@@ -30,9 +31,7 @@ class WechatController extends Controller
                       . $this->withEvent($message->Event, $message);
                   break;
                 case 'text':
-                        $text = new Text();
-                        $text->content = '收到文字消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的文字消息 <'.date('Y-m-d H:i:s', $createTime).'>'
-                            . '文本消息内容：' . $message->Content;
+                        $text = $this->withText($message->Content, $message);
                         return $text;
                   break;
                 case 'image':
@@ -45,8 +44,8 @@ class WechatController extends Controller
                         $video = new Video();
                         $video->media_id = $message->MediaId;
                         $video->ThumbMediaId = $message->ThumbMediaId;
-                        $video->title = $message->Title;
-                        $video->description = $message->Description;
+                        $video->title = '测试数据标题';
+                        $video->description = '测试数据描述';
                         return $video;
                     break;
                 case 'voice':
@@ -63,31 +62,6 @@ class WechatController extends Controller
                   return '收到链接消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的链接消息 <'.date('Y-m-d H:i:s', $createTime).'>'
                             . '消息标题：' . $message->Title . ' 消息描述：' . $message->Description . '消息链接：' . $message->Url;
                   break;
-                case 'tw':
-                    $news = new News([
-                        'title' => '测试数据',
-                        'description' => '测试数据描述',
-                        'url' => 'www.baidu.com',
-                        'image' => 'https://i.pximg.net/c/600x600/img-master/img/2017/09/23/21/31/40/65101437_p0_master1200.jpg'
-                    ]);
-                    return $news;
-                    break;
-                case 'wz':
-                    $article = new Article([
-                        'title' => 'DkfWeChat',
-                        'author' => 'dkf',
-                        'content' => '测试数据,测试数据,测试数据,测试数据,测试数据,测试数据,测试数据,测试数据.',
-                        'thumb_media_id' => '04c3f38da9773912145e5d4afb198618377ae2e7.jpg',
-                        'digest' => '图文消息的摘要，仅有单图文消息才有摘要，多图文此处为空',
-                        'source_url' => 'www.dengkangfa.com',
-                        'show_cover' => 1
-                    ]);
-                    return $article;
-                    break;
-                case 'sc':
-                    $material = new Material('image', '04c3f38da9773912145e5d4afb198618377ae2e7.jpg');
-                    return $material;
-                    break;
                 default:
                   return '收到其他消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的其他消息 <'.date('Y-m-d H:i:s', $createTime).'>';
                   break;
@@ -120,6 +94,46 @@ class WechatController extends Controller
                 break;
             default:
                 return '其他事件';
+        }
+    }
+
+    public function withText($text, $message)
+    {
+        $toUserName = $message->ToUserName;
+        $fromUserName = $message->FromUserName;
+        $createTime = $message->CreateTime;
+        switch ($text) {
+            case 'tw':
+                $news = new News([
+                    'title' => '测试数据',
+                    'description' => '测试数据描述',
+                    'url' => 'www.baidu.com',
+                    'image' => 'https://i.pximg.net/c/600x600/img-master/img/2017/09/23/21/31/40/65101437_p0_master1200.jpg'
+                ]);
+                return $news;
+                break;
+            case 'wz':
+                $article = new Article([
+                    'title' => 'DkfWeChat',
+                    'author' => 'dkf',
+                    'content' => '测试数据,测试数据,测试数据,测试数据,测试数据,测试数据,测试数据,测试数据.',
+                    'thumb_media_id' => '04c3f38da9773912145e5d4afb198618377ae2e7.jpg',
+                    'digest' => '图文消息的摘要，仅有单图文消息才有摘要，多图文此处为空',
+                    'source_url' => 'www.dengkangfa.com',
+                    'show_cover' => 1
+                ]);
+                return $article;
+                break;
+            case 'sc':
+                $material = new Material('image', '04c3f38da9773912145e5d4afb198618377ae2e7.jpg');
+                return $material;
+                break;
+            default:
+                $text = new Text();
+                $text->content = '收到文字消息：收到'.$fromUserName.' 发送给 '.$toUserName.'的文字消息 <'.date('Y-m-d H:i:s', $createTime).'>'
+                    . '文本消息内容：' . $message->Content;
+                return $text;
+                break;
         }
     }
 }
